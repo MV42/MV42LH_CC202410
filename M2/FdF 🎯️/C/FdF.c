1368aed672446788d852c8c7868e364a8db297d1
mavander <mavander@student.42lehavre.fr>
2025-02-13 18:28:26 +0100

    .

--- M2/FdF 🎯️/C/FdF.c
+++ M2/FdF 🎯️/C/FdF.c
@@ -21,6 +21,27 @@ typedef struct s_data
 	int		endian;
 }	t_data;
 
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
+}
+
+void	make_line(t_data **img, int start[2], int end[2], int color)
+{
+	int	x;
+	int	y;
+
+	x = start[0];
+	y = start[1];
+	while (x != end[0] && y != end[1])
+	{
+		
+	}
+	my_mlx_pixel_put(img, x, y, color);
+}
 int	main(void)
 {
 	void	*mlx;
@@ -32,5 +53,6 @@ int	main(void)
 	img.img = mlx_new_image(mlx, 200, 100);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel,
 			&img.line_length, &img.endian);
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
 	mlx_loop(mlx);
 }
