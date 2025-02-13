5f2d328428cdbef0731c47fdcbcee4f239931bb8
mavander <mavander@student.42lehavre.fr>
2025-02-13 17:11:08 +0100

    .

--- M2/FdF 🎯️/C/FdF.c
+++ M2/FdF 🎯️/C/FdF.c
@@ -12,14 +12,25 @@
 
 #include "../H/FdF.h"
 
+typedef struct s_data
+{
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}	t_data;
+
 int	main(void)
 {
 	void	*mlx;
 	void	*mlx_win;
-	void	*img;
+	t_data	img;
 
 	mlx = mlx_init();
 	mlx_win = mlx_new_window(mlx, 1000, 500, "Fil De Fer");
-	img = mlx_new_image(mlx, 200, 100);
+	img.img = mlx_new_image(mlx, 200, 100);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel,
+			&img.line_length, &img.endian);
 	mlx_loop(mlx);
 }
