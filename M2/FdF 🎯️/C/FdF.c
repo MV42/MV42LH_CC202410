51203dbee2e6332744061e036357400fc8337a0f
mavander <mavander@student.42lehavre.fr>
2025-02-13 15:23:07 +0100

    Push

---
+++ M2/FdF 🎯️/C/FdF.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mavander <mavander@student.42lehavre.fr>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/21 21:42:42 by mavander          #+#    #+#             */
+/*   Updated: 2024/12/21 21:42:42 by mavander         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../H/FdF.h"
+
+int	main(void)
+{
+	void	*mlx;
+	void	*mlx_win;
+	void	*img;
+
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 1000, 500, "Fil De Fer");
+	img = mlx_new_image(mlx, 200, 100);
+	mlx_loop(mlx);
+}
